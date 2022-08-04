e53115400630d4ebe54d4496874de82b74b5b1cc
Michael Milewski <saramic@gmail.com>
2022-08-05 08:26:55 +1000

    Landing page with new and slideshow message links

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resource :messages, only: %i[new] do
+    get :slideshow
+  end
 
   # a test only route used by spec/features/it_works_spec.rb
   get "test_root", to: "rails/welcome#index", as: "test_root_rails"
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "messages#index"
 end
