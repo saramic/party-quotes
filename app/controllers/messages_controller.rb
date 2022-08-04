ee4ec55c5b5dd491e4b744608ed21da1ad96ecd7
Michael Milewski <saramic@gmail.com>
2022-08-05 09:28:25 +1000

    Basic happy path picture with message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,6 +40,6 @@ class MessagesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def message_params
-    params.require(:message).permit(:text, :name)
+    params.require(:message).permit(:text, :name, :picture)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@
 class Message < ApplicationRecord
   validates :text, length: {minimum: 5, allow_blank: false}
   validates :name, length: {minimum: 3, allow_blank: false}
+
+  has_one_attached :picture
 end
