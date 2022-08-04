947650173ac7f5036faac71d22931848468a3dae
Michael Milewski <saramic@gmail.com>
2022-08-05 08:19:56 +1000

    Quick test of rails root it works page

---
+++ spec/support/capybara.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "capybara/rspec"
+require "capybara/rails"
+require "capybara-inline-screenshot/rspec"
+
+Capybara.javascript_driver = :selenium_chrome
+
+Capybara::Screenshot.register_driver(:selenium_chrome) do |driver, path|
+  driver.browser.save_screenshot(path)
+end
+
+Capybara.add_selector(:qa) do
+  css { |name| %([data-testid="#{name}"]) }
+end
