1173cd7247681294a0ddfbfa4515280e6125055b
Michael Milewski <saramic@gmail.com>
2022-08-07 08:50:38 +1000

    Max message length 260 chars like a tweet

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Message < ApplicationRecord
-  validates :text, length: {minimum: 5, allow_blank: false}
+  validates :text, length: {minimum: 5, maximum: 260, allow_blank: false}
   validates :name, length: {minimum: 3, allow_blank: false}
 
   has_one_attached :picture
