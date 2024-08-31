8865851ac74e562dcf9d62e045620d17af3c7ec9
Michael Milewski <saramic@gmail.com>
2024-08-31 11:15:12 +1000

    Simplify minimum validation on submit

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Message < ApplicationRecord
-  validates :text, length: {minimum: 5, maximum: 260, allow_blank: false}
-  validates :name, length: {minimum: 3, allow_blank: false}
+  validates :text, length: {minimum: 0, maximum: 260, allow_blank: false}
+  validates :name, length: {minimum: 0, allow_blank: false}
 
   has_one_attached :picture
 end
